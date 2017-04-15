55b10efdab452d626db79c6fb7656176708d6ebd
Mateusz Czaplinski <czapkofan@gmail.com>
2017-04-15 22:20:25 +0000

    cut out code not necessary for Lua
    Remove Web-related code fragments, not needed for planned Lua port of
    the code. Also, rename the code file for easier use.

--- asciimath.js
+++ asciimath.js
@@ -1,16 +1,11 @@
 /*
-ASCIIMathML.js
-==============
-This file contains JavaScript functions to convert ASCII math notation
-and (some) LaTeX to Presentation MathML. The conversion is done while the 
-HTML page loads, and should work with Firefox and other browsers that can
-render MathML.
+asciimath.js
+============
+This file contains JavaScript functions to convert ASCII math notation and
+(some) LaTeX to Presentation MathML (should work with Firefox and other
+browsers that can render MathML).
 
-Just add the next line to your HTML page with this file in the same folder:
-
-<script type="text/javascript" src="ASCIIMathML.js"></script>
-
-Version 2.2 Mar 3, 2014.
+Version 2.2 Mar 3, 2014. [cut by akavel - Mateusz Czaplinski, 2017]
 Latest version at https://github.com/mathjax/asciimathml
 If you use it on a webpage, please send the URL to jipsen@chapman.edu
 
@@ -42,7 +37,7 @@ var mathfontsize = "1em";      // change to e.g. 1.2em for larger math
 var mathfontfamily = "serif";  // change to "" to inherit (works in IE) 
                                // or another family (e.g. "arial")
 var automathrecognize = false; // writing "amath" on page makes this true
-var checkForMathML = true;     // check if browser can display MathML
+//var checkForMathML = true;     // check if browser can display MathML
 var notifyIfNoMathML = true;   // display note at top if no MathML capability
 var alertIfNoMathML = false;   // show alert box if no MathML capability
 var translateOnLoad = true;    // set to false to do call translators from js 
@@ -56,105 +51,13 @@ var fixphi = true;  		//false to return to legacy phi/varphi mapping
 
 /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-var isIE = (navigator.appName.slice(0,9)=="Microsoft");
+var isIE = false;
 var noMathML = false, translated = false;
-
-if (isIE) { // add MathPlayer info to IE webpages
-  document.write("<object id=\"mathplayer\"\
-  classid=\"clsid:32F66A20-7614-11D4-BD11-00104BD3F987\"></object>");
-  document.write("<?import namespace=\"m\" implementation=\"#mathplayer\"?>");
-}
-
-// Add a stylesheet, replacing any previous custom stylesheet (adapted from TW)
-function setStylesheet(s) {
-	var id = "AMMLcustomStyleSheet";
-	var n = document.getElementById(id);
-	if(document.createStyleSheet) {
-		// Test for IE's non-standard createStyleSheet method
-		if(n)
-			n.parentNode.removeChild(n);
-		// This failed without the &nbsp;
-		document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeEnd","&nbsp;<style id='" + id + "'>" + s + "</style>");
-	} else {
-		if(n) {
-			n.replaceChild(document.createTextNode(s),n.firstChild);
-		} else {
-			n = document.createElement("style");
-			n.type = "text/css";
-			n.id = id;
-			n.appendChild(document.createTextNode(s));
-			document.getElementsByTagName("head")[0].appendChild(n);
-		}
-	}
-}
-
-setStylesheet("#AMMLcloseDiv \{font-size:0.8em; padding-top:1em; color:#014\}\n#AMMLwarningBox \{position:absolute; width:100%; top:0; left:0; z-index:200; text-align:center; font-size:1em; font-weight:bold; padding:0.5em 0 0.5em 0; color:#ffc; background:#c30\}");
 
 function init(){
 	var msg, warnings = new Array();
-	if (document.getElementById==null){
-		alert("This webpage requires a recent browser such as Mozilla Firefox");
-		return null;
-	}
-	if (checkForMathML && (msg = checkMathML())) warnings.push(msg);
-	if (warnings.length>0) displayWarnings(warnings);
 	if (!noMathML) initSymbols();
 	return true;
-}
-
-function checkMathML(){
-  if (navigator.appName.slice(0,8)=="Netscape") 
-    if (navigator.appVersion.slice(0,1)>="5") noMathML = null;
-    else noMathML = true;
-  else if (navigator.appName.slice(0,9)=="Microsoft")
-    try {
-        var ActiveX = new ActiveXObject("MathPlayer.Factory.1");
-        noMathML = null;
-    } catch (e) {
-        noMathML = true;
-    }
-  else if (navigator.appName.slice(0,5)=="Opera") 
-    if (navigator.appVersion.slice(0,3)>="9.5") noMathML = null;
-  else noMathML = true;
-//noMathML = true; //uncomment to check
-  if (noMathML && notifyIfNoMathML) {
-    var msg = "To view the ASCIIMathML notation use Internet Explorer + MathPlayer or Mozilla Firefox 2.0 or later.";
-    if (alertIfNoMathML)
-       alert(msg);
-    else return msg;
-  }
-}
-
-function hideWarning(){
-	var body = document.getElementsByTagName("body")[0];
-	body.removeChild(document.getElementById('AMMLwarningBox'));
-	body.onclick = null;
-}
-
-function displayWarnings(warnings) {
-  var i, frag, nd = createElementXHTML("div");
-  var body = document.getElementsByTagName("body")[0];
-  body.onclick=hideWarning;
-  nd.id = 'AMMLwarningBox';
-  for (i=0; i<warnings.length; i++) {
-	frag = createElementXHTML("div");
-	frag.appendChild(document.createTextNode(warnings[i]));
-	frag.style.paddingBottom = "1.0em";
-	nd.appendChild(frag);
-  }
-  nd.appendChild(createElementXHTML("p"));
-  nd.appendChild(document.createTextNode("For instructions see the "));
-  var an = createElementXHTML("a");
-  an.appendChild(document.createTextNode("ASCIIMathML"));
-  an.setAttribute("href","http://www.chapman.edu/~jipsen/asciimath.html");
-  nd.appendChild(an);
-  nd.appendChild(document.createTextNode(" homepage"));
-  an = createElementXHTML("div");
-  an.id = 'AMMLcloseDiv';
-  an.appendChild(document.createTextNode('(click anywhere to close this warning)'));
-  nd.appendChild(an);
-  var body = document.getElementsByTagName("body")[0];
-  body.insertBefore(nd,body.childNodes[0]);
 }
 
 function translate(spanclassAM) {
@@ -1081,46 +984,13 @@ function AMprocessNode(n, linebreaks, spanclassAM) {
   }
 }
 
-function generic(){
-  if(!init()) return;
-  if (translateOnLoad) {
-      translate();
-  }
-};
-//setup onload function
-if(typeof window.addEventListener != 'undefined'){
-  //.. gecko, safari, konqueror and standard
-  window.addEventListener('load', generic, false);
-}
-else if(typeof document.addEventListener != 'undefined'){
-  //.. opera 7
-  document.addEventListener('load', generic, false);
-}
-else if(typeof window.attachEvent != 'undefined'){
-  //.. win/ie
-  window.attachEvent('onload', generic);
-}else{
-  //.. mac/ie5 and anything else that gets this far
-  //if there's an existing onload function
-  if(typeof window.onload == 'function'){
-    //store it
-    var existing = onload;
-    //add new onload handler
-    window.onload = function(){
-      //call existing onload function
-      existing();
-      //call generic onload function
-      generic();
-    };
-  }else{
-    window.onload = generic;
-  }
-}
-
 //expose some functions to outside
 asciimath.newcommand = newcommand;
 asciimath.newsymbol = newsymbol;
 asciimath.AMprocesssNode = AMprocessNode;
 asciimath.parseMath = parseMath;
 asciimath.translate = translate;
+//expose more [akavel]
+asciimath.processNodeR = processNodeR;
+asciimath.strarr2docFrag = strarr2docFrag;
 })();
