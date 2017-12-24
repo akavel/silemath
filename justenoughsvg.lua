3c3030fb69bc1c461ae46e1f02db845a90680c4f
Mateusz Czapliński <czapkofan@gmail.com>
2017-12-24 20:47:22 +0100

    WIP justenoughsvg parse_tree

--- justenoughsvg.lua
+++ justenoughsvg.lua
@@ -11,3 +11,80 @@
 -- TODO: optionally, as intermediate alternative,
 -- maybe render the nodes in "live mode" in ZeroBrane
 -- Studio into some graphics engine?
+
+-- NOTE: this parser is expected to work only on SVGs
+-- emitted by svgmath, and as such makes many simplifying
+-- assumptions about the input.
+
+-- wrap_getc returns an object with methods: getc, ungetc,
+-- error.
+function wrap_getc(getc)
+	return {
+		ungetc = function(self, c)
+			self.c = c
+		end,
+		getc = function(self)
+			if self.c then
+				local c = self.c
+				self.c = nil
+				return c
+			end
+			return getc()
+		end,
+		error = function(self, ...)
+			-- TODO: show position in input
+			error(...)
+		end,
+	}
+end
+
+-- parse_tree returns a tree of objects representing
+-- parsed SVG, or raises error. The g should be a result
+-- of wrap_getc.
+function parse_tree(g)
+	local node = {}
+	-- opening '<'
+	local c = g:getc()
+	if c ~= '<' then g:error('expected "<", got "'..c..'"') end
+	-- node name (usually namespace:name)
+	local n = {}
+	while true do
+		local c = g:getc()
+		if c >= 'a' and c <= 'z' then
+			n[#n+1] = c
+		elseif c == ':' then
+			-- namespace - discard
+			n = {}
+		elseif c == ' ' or c == '>' or c == '/' then
+			-- end of node name
+			g:ungetc(c)
+			break
+		else
+			g:error('unexpected character "'..c..'" in node name')
+		end
+	end
+	node.name = table.concat(n, '')
+	-- optional space-separated attributes
+	while true do
+		local c = g:getc()
+		if c ~= ' ' then
+			g:ungetc(c)
+			break
+		end
+		if not node.attrs then node.attrs = {} end
+		local name, value = parse_attr(g)
+		node.attrs[name] = value
+	end
+	-- does the element have children?
+	local c = g:getc()
+	if c == '/' then
+		-- no children
+		local c = g:getc()
+		if c ~= '>' then
+			g:error('expected "/>", got "/'..c..'"')
+		end
+		return node
+	end
+	-- parse children
+end
+
